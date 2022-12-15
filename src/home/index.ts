270b039dc0d1a4d34efdfd3a2194d8ebee4fcc5d
spoon-inoue <h-inoue@studio-spoon.co.jp>
2022-12-15 18:46:38 +0900

    add font

--- src/home/index.ts
+++ src/home/index.ts
@@ -1,7 +1,16 @@
 import { TCanvas } from './webgl/TCanvas'
+import { gui } from './webgl/utils/gui'
 
 const canvas = new TCanvas(document.body)
 
 window.addEventListener('beforeunload', () => {
 	canvas.dipose()
 })
+
+const obj = {
+	toggleTitleColor: () => {
+		const head = document.querySelector<HTMLElement>('.text__head')!
+		head.classList.toggle('text__head--light')
+	},
+}
+gui.add(obj, 'toggleTitleColor').name('toggle title color')
